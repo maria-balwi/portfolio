496fbb9c74d8d52d6116ff8e0dc241cba11389e0
maria-balwi <patricereyes.MPR@gmail.com>
2025-07-02 19:10:46 +0800

    chore(layout): clean up unused lines

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router'
-// import Home from './pages/Home'
 // import About from './pages/About'
 // import Portfolio from './pages/Portfolio'
 // import Contact from './pages/Contact'
